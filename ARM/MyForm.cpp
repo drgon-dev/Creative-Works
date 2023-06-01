c8aa35a1b2bf9abdba774de2723391ec049a17f4
Maks Golovin <70146981+drgon-dev@users.noreply.github.com>
2023-06-01 10:32:11 +0500

    Add files via upload

---
+++ ARM/MyForm.cpp
@@ -0,0 +1,12 @@
+#include "MyForm1.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	ARM::MyForm1 form;
+	Application::Run(% form);
+}
